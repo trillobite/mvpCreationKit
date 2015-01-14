0cf173bcc0d4b796dc83cac087f28a98e6841d5a
trillobite <jparnell0@gmail.com>
2015-01-14 09:56:10 -0800

    changed to AJAX, all GET requests

--- scripts/connection.js
+++ scripts/connection.js
@@ -7,46 +7,102 @@ var $db = {
 
 	getCanvas: function(pricingFormID, photographerID) {
 		var dfd = new $.Deferred();
-		var url = "http://www.mypicday.com/Handlers/CanvasGetPricingFormCanvases.aspx?PricingFormID=" + pricingFormID + ",&PhotographerID=" + photographerID;
-		$sql($db.preventCache(url)).get(function(data) {
+		var request = "http://www.mypicday.com/Handlers/CanvasGetPricingFormCanvases.aspx?PricingFormID=" + pricingFormID + ",&PhotographerID=" + photographerID;
+
+                $.ajax({
+                        type: 'GET',
+                        dataType: 'jsonp',
+                        url: request,
+                        success: function(data) {
+                                dfd.resolve(data);
+                        },              
+                        error: function(data) {
+                                dfd.resolve(data);
+                        },
+                        
+                });
+
+		/*$sql($db.preventCache(url)).get(function(data) {
 			dfd.resolve(data); //returns the resulting data within the resolve.
-		});
+		});*/
 		return dfd.promise();
 	},
 
 	mkNwCanDef: function(PricingFormID, PricingFormCanvasDescription, PhotographerID) {
 		var dfd = new $.Deferred();
-		var url = "http://www.mypicday.com/Handlers/CanvasNewPricingFormCanvas.aspx?";
-		url += 'PricingFormID=' + PricingFormID;
-		url += ('&' + 'PricingFormCanvasDescription=' + PricingFormCanvasDescription);
-		url += ('&' + 'PhotographerID=' + PhotographerID);
-		$sql($db.preventCache(url)).get(function(data) {
+		var request = "http://www.mypicday.com/Handlers/CanvasNewPricingFormCanvas.aspx?";
+		request += 'PricingFormID=' + PricingFormID;
+		request += ('&' + 'PricingFormCanvasDescription=' + PricingFormCanvasDescription);
+		request += ('&' + 'PhotographerID=' + PhotographerID);
+
+                $.ajax({
+                        type: 'GET',
+                        dataType: 'jsonp',
+                        url: request,
+                        success: function(data) {
+                                dfd.resolve(data);
+                        },              
+                        error: function(data) {
+                                dfd.resolve(data);
+                        },
+                        
+                });
+
+		/*$sql($db.preventCache(url)).get(function(data) {
 			dfd.resolve(data); //returns the resulting data within the resolve.
-		});
+		});*/
 		return dfd.promise();
 	},
 
 	getCanJson: function(PricingFormCanvasID, PhotographerID) {
 		var dfd = new $.Deferred();
-		var url = "http://www.mypicday.com/Handlers/CanvasGetPricingFormCanvas.aspx?";
-		url += 'PricingFormCanvasID=' + PricingFormCanvasID;
-		url += '&PhotographerID=' + PhotographerID;
-		$sql($db.preventCache(url)).get(function(data) {
+		var request = "http://www.mypicday.com/Handlers/CanvasGetPricingFormCanvas.aspx?";
+		request += 'PricingFormCanvasID=' + PricingFormCanvasID;
+		request += '&PhotographerID=' + PhotographerID;
+
+                $.ajax({
+                        type: 'GET',
+                        dataType: 'jsonp',
+                        url: request,
+                        success: function(data) {
+                                dfd.resolve(data);
+                        },              
+                        error: function(data) {
+                                dfd.resolve(data);
+                        },
+                        
+                });
+
+		/*$sql($db.preventCache(url)).get(function(data) {
 			dfd.resolve(data); //returns the resulting data within the resolve.
-		});
+		});*/
 		return dfd.promise();
 	},	
 	
 	svCanJson: function(PricingFormCanvasID, PhotographerID, DesignData) {
 		var dfd = new $.Deferred();
-		var url = 'http://www.mypicday.com/Handlers/CanvasSavePricingFormCanvas.aspx?';
-		url += 'PricingFormCanvasID=' + PricingFormCanvasID;
-		url += '&PhotographerID=' + PhotographerID;
-		url += '&DesignData=' + DesignData;
-		console.log(url);
-		$sql($db.preventCache(url)).get(function(data) {
-			dfd.resolve(data); //returns the resulting data within the resolve.
+		var request = 'http://www.mypicday.com/Handlers/CanvasSavePricingFormCanvas.aspx?';
+		request += 'PricingFormCanvasID=' + PricingFormCanvasID;
+		request += '&PhotographerID=' + PhotographerID;
+		request += '&DesignData=' + DesignData;
+		console.log(request);
+
+		$.ajax({
+			type: 'GET',
+			dataType: 'jsonp',
+			url: request,
+			success: function(data) {
+				dfd.resolve(data);
+			},		
+			error: function(data) {
+				dfd.resolve(data);
+			},
+			
 		});
+
+		/*$sql($db.preventCache(request)).get(function(data) {
+			dfd.resolve(data); //returns the resulting data within the resolve.
+		});*/
 		return dfd.promise();
 	},
 };
